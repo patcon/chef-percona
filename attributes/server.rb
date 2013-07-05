3a9c2ed22264efffbbd4702a46abbee97aa1a535
Patrick Connolly <patrick@myplanetdigital.com>
2013-07-05 16:39:09 -0400

    Added multi-version logic for percona::server attr file.

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,10 +1,24 @@
+version = node['percona']['version'].to_s
 normal['mysql']['use_upstart'] = false
 
 case node["platform_family"]
 when "debian"
-  normal['mysql']['server']['packages'] = %w{percona-server-server}
+  case version
+  when "5.0", "5.1", "5.5", "5.6"
+    normal['mysql']['server']['packages'] = %W{percona-server-server-#{version}}
+  when "latest"
+    normal['mysql']['server']['packages'] = %w{percona-server-server}
+  end
 when "rhel"
-  normal['mysql']['server']['packages'] = %w{Percona-Server-server-55}
   normal['mysql']['service_name'] = "mysql"
   normal['mysql']['pid_file'] = ""
+
+  case version
+  when "5.0"
+    normal['mysql']['server']['packages'] = %W{Percona-SQL-server-#{version.delete('.')}}
+  when "5.1", "5.5", "5.6"
+    normal['mysql']['server']['packages'] = %W{Percona-Server-server-#{version.delete('.')}}
+  when "latest"
+    normal['mysql']['server']['packages'] = %w{Percona-Server-server-56}
+  end
 end
