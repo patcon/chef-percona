d351852fc3b14b66482cad106a54ed34fd474175
Patrick Connolly <patrick@myplanetdigital.com>
2013-07-04 21:14:52 -0400

    Makes percona::client version tunable.

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,6 +1,19 @@
 case node["platform_family"]
 when "debian"
-  normal['mysql']['client']['packages'] = %w{libmysqlclient-dev percona-server-client}
+  case node['percona']['version']
+  when "5.0", "5.1", "5.5", "5.6"
+    normal['mysql']['client']['packages'] = %W{percona-server-client-#{node['percona']['version']}}
+  when "latest"
+    normal['mysql']['client']['packages'] = %w{percona-server-client}
+  end
+
 when "rhel"
-  normal['mysql']['client']['packages'] = %w{Percona-Server-client-55}
+  case node['percona']['version']
+  when "5.0"
+    normal['mysql']['client']['packages'] = %W{Percona-SQL-client-#{node['percona']['version'].delete('.')}}
+  when "5.1", "5.5", "5.6"
+    normal['mysql']['client']['packages'] = %W{Percona-Server-client-#{node['percona']['version'].delete('.')}}
+  when "latest"
+    normal['mysql']['client']['packages'] = %w{Percona-Server-client-56}
+  end
 end
--- attributes/package_repo.rb
+++ attributes/package_repo.rb
@@ -1,3 +1,5 @@
+default['percona']['version'] = "latest"
+
 default['percona']['apt_uri'] = 'http://repo.percona.com/apt'
 default['percona']['apt_keyserver'] = 'keys.gnupg.net'
 default['percona']['apt_key'] = '1C4CBDCDCD2EFD2A'
